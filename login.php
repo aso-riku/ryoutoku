94e3507f1e849ff88d9697bc798d01b969b37773
Ryotoku <2201024@s.asojuku.ac.jp>
2025-05-28 11:46:55 +0900

    ログインしちゃいました

--- login.php
+++ login.php
@@ -1,2 +1,17 @@
-
-     
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>ログイン</h1>
+    <form action="" method="post">
+    <p>
+        ユーザー名：<input type="text" name="username"><br>
+        パスワード：<input type="text" name="password"><br>
+        <button type="submit" name="action" value="send">送信</button>
+</form>
+</body>
+</html>
