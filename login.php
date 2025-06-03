2330b913822332f1364b8ebc0e541196341e5ec6
aso-riku <2401334@s.asojuku.ac.jp>
2025-06-03 10:36:57 +0900

    login

--- login.php
+++ login.php
@@ -10,8 +10,8 @@
     <h1>ログイン</h1>
     <form action="check.php" method="post">
     <p>
-        ユーザー名：<input type="text" name="username"><br>
-        パスワード：<input type="text" name="password"><br>
+        ユーザー名：<input type="text" name="username" value="daichi"><br>
+        パスワード：<input type="text" name="password" value="1111"><br>
         <button type="submit" name="action" value="send">送信</button>
 </form>
 </body>
